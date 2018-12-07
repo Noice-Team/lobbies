9f78660a13efc5d8ac1de38ec3aeb8db8fbb8bee
Kamule <benjamin.herbomez@gmail.com>
2018-12-07 22:37:33 +0100

    Fix cors ?

--- src/main/java/com/noice/xxxx/lobbies/app/AppApplication.java
+++ src/main/java/com/noice/xxxx/lobbies/app/AppApplication.java
@@ -14,14 +14,4 @@ public class AppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/"+LobbyController.NAME).allowedOrigins("*");
-			}
-		};
-	}
 }
